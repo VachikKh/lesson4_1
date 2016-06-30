1e1efc181cc642ddaecbe1aed77d5bb7bc854e56
VachikKh <vachik19941010@gmail.com>
2016-06-30 16:08:02 +0400

    first commit

---
+++ ex2_wr_file.py
@@ -0,0 +1,4 @@
+f=open("write.txt","w")
+f.write("This is Sparta")
+f.close()
+
---
+++ lesson4.py
@@ -0,0 +1,18 @@
+#f=open("data.txt","rb")
+#s=f.read()
+#print(s)
+#f.close()
+
+#f=open("example.txt")
+#print(f.read(1))
+
+#for line in open("example.txt"):
+ #   if "3 lines" in line:
+  #      print(line)
+
+ #  for line in open ("example.txt"):
+  #     x=line.strip()
+       #print(x)
+
+
+
